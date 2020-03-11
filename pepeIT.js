db4420b1200cfff9e38144a1e089e77390dedc8d
Max S <creatingalong@gmail.com>
2020-03-10 23:39:35 -0500

    added more comments and added array of pepe image links

--- pepeIT.js
+++ pepeIT.js
@@ -1,4 +1,39 @@
+// defines the html element tag
 var imgs = document.getElementsByTagName("img");
+
+// an array containing links to the pepe images
+var pepeUrls = [
+    "https://77extracts.com/images/pepe/0001.png",
+    "https://77extracts.com/images/pepe/0002.png",
+    "https://77extracts.com/images/pepe/0003.png",
+    "https://77extracts.com/images/pepe/0004.png",
+    "https://77extracts.com/images/pepe/0005.png",
+    "https://77extracts.com/images/pepe/0006.png",
+    "https://77extracts.com/images/pepe/0007.png",
+    "https://77extracts.com/images/pepe/0008.png",
+    "https://77extracts.com/images/pepe/0009.png",
+    "https://77extracts.com/images/pepe/0010.png",
+    "https://77extracts.com/images/pepe/0011.png",
+    "https://77extracts.com/images/pepe/0012.png",
+    "https://77extracts.com/images/pepe/0013.png",
+    "https://77extracts.com/images/pepe/0014.png",
+    "https://77extracts.com/images/pepe/0015.png",
+    "https://77extracts.com/images/pepe/0016.png",
+    "https://77extracts.com/images/pepe/0017.png",
+    "https://77extracts.com/images/pepe/0018.png",
+    "https://77extracts.com/images/pepe/0019.png",
+    "https://77extracts.com/images/pepe/0020.png",
+    "https://77extracts.com/images/pepe/0021.png",
+    "https://77extracts.com/images/pepe/0022.png",
+    "https://77extracts.com/images/pepe/0023.png",
+    "https://77extracts.com/images/pepe/0024.png",
+    "https://77extracts.com/images/pepe/0025.png",
+    "https://77extracts.com/images/pepe/0026.png",
+    "https://77extracts.com/images/pepe/0027.png",
+    "https://77extracts.com/images/pepe/0028.png",
+    "https://77extracts.com/images/pepe/0029.png",
+    "https://77extracts.com/images/pepe/0030.png"
+];
 
 // sets the images
 for (var i=0, l=imgs.length;i<l;i++) {
